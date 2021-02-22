01d43620005cb39ab2daf3c6ad741e33083e0bf6
dbmisrael <819f4675645b91661821c86737a6398d5d1b5792>
2021-02-22 14:00:47 +0200

    Added C4 + C5 to table_1

--- DEV/Dev_Env_1/DevDB/dbo/Table/Table_1/Script.sql
+++ DEV/Dev_Env_1/DevDB/dbo/Table/Table_1/Script.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[Table_1] (
     [C1] NCHAR (10) NULL,
     [C2] NCHAR (10) NULL,
-    [C3] NCHAR (10) NULL
+    [C3] NCHAR (10) NULL,
+    [C4] NCHAR (10) NULL,
+    [C5] NCHAR (10) NULL
 );
 
 
