0831bf3ad5d7deab81907b77c1796103639e8218
dbmisrael <819f4675645b91661821c86737a6398d5d1b5792>
2021-02-22 14:54:58 +0200

    Added column C7 to table_1

--- DEV/Dev_Env_1/DevDB/dbo/Table/Table_1/Script.sql
+++ DEV/Dev_Env_1/DevDB/dbo/Table/Table_1/Script.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Table_1] (
     [C3] NCHAR (10) NULL,
     [C4] NCHAR (10) NULL,
     [C5] NCHAR (10) NULL,
-    [C6] NCHAR (10) NULL
+    [C6] NCHAR (10) NULL,
+    [C7] NCHAR (10) NULL
 );
 
 
