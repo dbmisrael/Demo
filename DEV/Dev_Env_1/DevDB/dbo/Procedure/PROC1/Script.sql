3e0703856c4f932834468491bb78e5b31c8dc820
dbmisrael <819f4675645b91661821c86737a6398d5d1b5792>
2021-02-23 09:44:16 +0200

    commit

--- DEV/Dev_Env_1/DevDB/dbo/Procedure/PROC1/Script.sql
+++ DEV/Dev_Env_1/DevDB/dbo/Procedure/PROC1/Script.sql
@@ -1,16 +1,11 @@
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE PROC1
+CREATE   PROCEDURE PROC1
 	
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	-- comment 1
+	-- comment 2
 END
 GO
 
