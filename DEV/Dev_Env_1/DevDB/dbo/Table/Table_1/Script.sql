c7777829bf90861d9d30686da7055c38be17f55d
dbmisrael <df1a051cb0794e0cefde575566a039c65b7f50e9>
2021-03-04 14:46:23 +0200

    add all

---
+++ DEV/Dev_Env_1/DevDB/dbo/Table/Table_1/Script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[Table_1] (
+    [C1] NCHAR (10) NULL,
+    [C2] NCHAR (10) NULL,
+    [C3] NCHAR (10) NULL,
+    [C4] NCHAR (10) NULL,
+    [C5] NCHAR (10) NULL,
+    [C6] NCHAR (10) NULL,
+    [C7] NCHAR (10) NULL,
+    [C8] NCHAR (10) NULL
+);
+
+
+GO
+
