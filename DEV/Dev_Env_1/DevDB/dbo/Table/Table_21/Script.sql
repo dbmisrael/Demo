b0bba3acc4023afb990021e18f24f8d6264c583c
israelys <361641e9abd160971b13cd3edddd62bf3f593d53>
2021-03-04 13:45:26 +0200

    test

---
+++ DEV/Dev_Env_1/DevDB/dbo/Table/Table_21/Script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Table_21] (
+    [C1] NCHAR (10) NULL
+);
+
+
+GO
+
--- DEV/Dev_Env_1/DevDB/dbo/Table/Table_5/Script.sql
+++ DEV/Dev_Env_1/DevDB/dbo/Table/Table_5/Script.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table_5] (
     [C1] NCHAR (10) NULL,
-    [C2] NCHAR (10) NULL
+    [C2] NCHAR (10) NULL,
+    [C4] NCHAR (10) NULL
 );
 
 
