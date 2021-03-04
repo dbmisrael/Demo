7dcd7a84a8f8a0eac09bcdb107bd2884bdeac621
dbmisrael <78854344+dbmisrael@users.noreply.github.com>
2021-03-04 11:53:16 +0200

    add all

---
+++ DEV/Dev_Env_1/DevDB/dbo/Procedure/PROC1/Script.sql
@@ -0,0 +1,11 @@
+CREATE   PROCEDURE PROC1
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	-- comment 2
+END
+GO
+
