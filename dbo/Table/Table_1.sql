31f19a077d9923a96edba00c6d3442b2eccf9088
israelys <361641e9abd160971b13cd3edddd62bf3f593d53>
2021-04-27 18:11:15 +0300

    commentos

---
+++ dbo/Table/Table_1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Table_1] (
+    [c1] NCHAR (10) NULL,
+    [f1] NCHAR (10) NULL,
+    [c2] NCHAR (10) NULL
+);
+
+
+GO
+
---
+++ dbo/Table/Table_2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Table_2] (
+    [cc] NCHAR (10) NULL,
+    [dd] NCHAR (10) NULL
+);
+
+
+GO
+
