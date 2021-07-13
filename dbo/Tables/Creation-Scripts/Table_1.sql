82aa844483dd453de997f95f157a042336327139
israelys <361641e9abd160971b13cd3edddd62bf3f593d53>
2021-07-13 09:33:03 +0300

    all

---
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Table_1] (
+    [c1] NCHAR (10) NULL,
+    [f1] NCHAR (10) NULL,
+    [c2] NCHAR (10) NULL,
+    [c3] NCHAR (10) NULL,
+    [c4] NCHAR (10) NULL,
+    [c5] NCHAR (10) NULL,
+    [c6] NCHAR (10) NULL
+);
+
+
+GO
+
